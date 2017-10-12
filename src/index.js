d7d9932a8b3b8d500225da4d21527bf327294d12
HenkBourgonje <bourgonjehenk@gmail.com>
2017-10-12 10:05:58 +0300

    First working version of API server that transfers data to the Mongo database

--- src/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express')
-var app = express()
-
-var mongojs = require('mongojs')
-var db = mongojs('localhost/CityOfHelsinki', ['people'])
-
-app.listen(3000, () => console.log('Running on port 3000...'))
