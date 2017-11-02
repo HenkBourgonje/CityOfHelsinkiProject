46bc43ee957e98703ec14a31c132bf5ce08060bb
HenkBourgonje <bourgonjehenk@gmail.com>
2017-11-02 13:29:25 +0200

    Working sorting options for the application list view

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
 const hbs = require('express-handlebars')
 const path = require('path');
@@ -8,23 +7,30 @@ const path = require('path');
 const mongo = require(path.resolve('db.js'))
 const db = mongo.get()
 
+// Static files location declaration
+app.use(express.static(path.join(__dirname, 'resources/css')))
+
+// Handlebars Express initialisation
 app.engine('handlebars', hbs({defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts'}))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'handlebars')
 
-// JSON bodyparser for every route
-app.use(bodyParser.json())
-
-// Imports
+// Importing functions from the exports module from functions.js
 let functions = require('./functions.js')
 
-// Homepage route
+/**
+ * Homepage route
+ */
 app.get('/', (req, res) => {
     functions.getDataFromDB(function (people) {
         res.render('home', {dashboard_content: people})
+
     })
 })
 
+/**
+ * Individual page route
+ */
 // Individual document retrieval route
 app.get('/individual/:id', (req, res) => {
     functions.findById(req.params.id, function (document) {
@@ -32,7 +38,9 @@ app.get('/individual/:id', (req, res) => {
     })
 })
 
-// Data submission route
+/**
+ * Data submission route
+ */
 app.post('/submit', (req, res) => {
     db.people.insert(req.body)
     res.send("OK")
