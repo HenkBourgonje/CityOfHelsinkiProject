8e1663eddfcf9217b7b7aeb99bba9f42c879211f
HenkBourgonje <bourgonjehenk@gmail.com>
2017-11-07 21:16:55 +0200

    Shows list of daycare preferences, basic Google Maps view, improved folder-structure (reaching for MVC)

--- src/server.js
+++ src/server.js
@@ -4,11 +4,14 @@ const hbs = require('express-handlebars')
 const path = require('path');
 
 // MongoDB Connection
-const mongo = require(path.resolve('db.js'))
+const mongo = require(path.join(__dirname, '/model/db.js'))
 const db = mongo.get()
 
+const bodyParser = require('body-parser')
+
 // Static files location declaration
-app.use(express.static(path.join(__dirname, 'resources/css')))
+app.use(express.static(path.join(__dirname, 'public/css')))
+app.use(express.static(path.join(__dirname)))
 
 // Handlebars Express initialisation
 app.engine('handlebars', hbs({defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts'}))
@@ -16,7 +19,10 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'handlebars')
 
 // Importing functions from the exports module from functions.js
-let functions = require('./functions.js')
+let functions = require('./model/functions.js')
+
+// JSON bodyparser for every route
+app.use(bodyParser.json())
 
 /**
  * Homepage route
@@ -24,7 +30,6 @@ let functions = require('./functions.js')
 app.get('/', (req, res) => {
     functions.getDataFromDB(function (people) {
         res.render('home', {dashboard_content: people})
-
     })
 })
 
@@ -33,9 +38,31 @@ app.get('/', (req, res) => {
  */
 // Individual document retrieval route
 app.get('/individual/:id', (req, res) => {
+    // Find all info by applicant ID
     functions.findById(req.params.id, function (document) {
-        res.render('individual', {individual_content: document})
+
+        let daycare_array = []
+
+        // Loops through array of kindergarten preference id's
+        for (let i = 0; i < document.kindergarten_choices.length; i++) {
+
+            // Gets information according to the kindergarten ID and puts it in array, it
+            // all has been covered, it renders the page.
+            functions.getDaycares(document.kindergarten_choices[i], function (daycare_document) {
+                daycare_array.push(daycare_document)
+
+                if (daycare_array.length === document.kindergarten_choices.length) {
+                    // console.log(daycare_array)
+                    res.render('individual', {personal_info: document, daycare_info: daycare_array})
+                }
+            })
+        }
     })
+})
+
+app.post('/application_submit/:child_id/:daycare_id', (req, res) => {
+    console.log('cool')
+    res.send('u')
 })
 
 /**
@@ -46,4 +73,7 @@ app.post('/submit', (req, res) => {
     res.send("OK")
 })
 
+/**
+ * Port initialisation
+ */
 app.listen(3000, () => console.log('Running on port 3000...'))
